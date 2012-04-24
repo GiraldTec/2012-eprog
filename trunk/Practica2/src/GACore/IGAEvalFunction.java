7459f98d893cb510c3c5bcdbbceefed2d37ac637
ritrix <ritrix@7db9f31f-8134-a458-1d93-559f5c0c0136>
2012-04-24 13:55:02 +0000

    - Borrados archivos que sobraban de /bin - Empezado la carga de estudiantes. - Eliminado eval funcs viejas.

--- trunk/Practica2/src/GACore/IGAEvalFunction.java
+++
@@ -1,9 +0,0 @@
-package GACore;
-
-public abstract class IGAEvalFunction {
-	protected int type;
-	public abstract Object evaluate(int numberParams,Double[] params); //devuelve el valor de la función evaluación
-	public abstract int calcCromLenghtGeneric(double precision);
-	public abstract int calcCromLenght(double precision);
-	public abstract double getType();
-}
--- trunk/Practica2/src/GACore/IGAMutator.java
+++
@@ -1,7 +0,0 @@
-package GACore;
-
-public abstract class IGAMutator {
-	
-	public abstract Boolean mutate(IGAGene gen);
-	
-}
