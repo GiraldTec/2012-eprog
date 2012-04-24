45bfcc4d484f4d608bbdae30b4fd4f3ebd823923
ritrix <ritrix@7db9f31f-8134-a458-1d93-559f5c0c0136>
2012-04-24 13:55:02 +0000

    - Borrados archivos que sobraban de /bin - Empezado la carga de estudiantes. - Eliminado eval funcs viejas.

--- Practica2/src/GACore/IGASelector.java
+++
@@ -1,5 +0,0 @@
-package GACore;
-
-public abstract class IGASelector{
-	public abstract IGACromosome[] select (IGACromosome[] pop, int pop_size) throws InstantiationException, IllegalAccessException;
-}
