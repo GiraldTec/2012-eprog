7c741dc0da21b831fb53e1477285f52c65453032
cacu2ggg <cacu2ggg@7db9f31f-8134-a458-1d93-559f5c0c0136>
2012-04-28 19:54:07 +0000

    Todavia sin rellenar el Mutator3Heuristic

--- trunk/Practica2/src/GA/GAMutator3Heuristic.java
+++ trunk/Practica2/src/GA/GAMutator3Heuristic.java
@@ -2,13 +2,21 @@ package GA;
 
 import GACore.IGAGene;
 import GACore.IGAMutator;
+import GACore.IGARandom;
+
 
 public class GAMutator3Heuristic extends IGAMutator{
 
 	@Override
 	public Boolean mutate(IGAGene gen, double prob) {
-		// TODO Auto-generated method stub
-		return null;
+		Boolean res = new Boolean(false);
+		if(IGARandom.getRDouble()<prob){
+			
+			
+			
+			res= !res;
+		}
+		return res;
 	}
 
 }
