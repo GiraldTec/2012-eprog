1694125084e52843b25e1c251b985a387458ff57
cacu2ggg <cacu2ggg@7db9f31f-8134-a458-1d93-559f5c0c0136>
2012-04-28 19:04:48 +0000

    GAMutator3Heuristic-Vacia. Voy a limpiar mi workspace y a ver si ahora eclipse funciona bien.

---
+++ trunk/Practica2/src/GA/GAMutator3Heuristic.java
@@ -0,0 +1,14 @@
+package GA;
+
+import GACore.IGAGene;
+import GACore.IGAMutator;
+
+public class GAMutator3Heuristic extends IGAMutator{
+
+	@Override
+	public Boolean mutate(IGAGene gen, double prob) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
