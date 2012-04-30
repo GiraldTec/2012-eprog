23c43ff3cc07439c4d621a55e2a6c7c75a73e73b
cacu2ggg <cacu2ggg@7db9f31f-8134-a458-1d93-559f5c0c0136>
2012-04-30 09:07:06 +0000

    Mutator Exchange Finiquitada

--- Practica2/src/GA/GAMutatorExchange.java
+++ Practica2/src/GA/GAMutatorExchange.java
@@ -9,9 +9,9 @@ public class GAMutatorExchange extends IGAMutator {
 		Boolean res = new Boolean(false);
 		if(IGARandom.getRDouble()<prob){
 			// Solo con dos numeros aleatorios
-			int auxA = IGARandom.getRInt(gen.getGen().length);
-			int auxB;
-			while ((auxB = IGARandom.getRInt(gen.getGen().length)) == auxA){}
+			int auxB = IGARandom.getRInt(gen.getGen().length - 1)+1;
+			int auxA = IGARandom.getRInt(auxB);
+			
 			
 			int auxiliar = gen.getGen()[auxA];
 			gen.getGen()[auxA]= gen.getGen()[auxB];
