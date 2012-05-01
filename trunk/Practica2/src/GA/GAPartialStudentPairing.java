3f452827837eb010b99fea524a17622a70116082
cacu2ggg <cacu2ggg@7db9f31f-8134-a458-1d93-559f5c0c0136>
2012-05-01 14:31:35 +0000

    PMX Cool

--- trunk/Practica2/src/GA/GAPartialStudentPairing.java
+++ trunk/Practica2/src/GA/GAPartialStudentPairing.java
@@ -25,9 +25,6 @@ public class GAPartialStudentPairing extends IGACross{
 		int[] padreGen1= parent1.getGene().getGen();
 		int[] padreGen2= parent2.getGene().getGen();
 		
-		checkear(padreGen1);
-		checkear(padreGen2);
-		
 		//inicializando los genesDescendientes
 		for(int i=0;i<genLength;i++){	genD1[i]=genD2[i]=-1;}
 		
@@ -46,38 +43,38 @@ public class GAPartialStudentPairing extends IGACross{
 			parejaH2[genD2[i]]=genD1[i];
 		}
 		
+		int aux1 = 0;
+		int aux2 = 0;
 		//cogemos las que no dan conflicto
 		for(int i=0;i<genLength;i++){
 			if(i<indexA | i>=indexB){
 				//Mirando el hijo1
 				if(!pertenece(padreGen1[i],genD1)){ // si no está ya en el gen, lo metemos
 					genD1[i]=padreGen1[i];
+				}else{
+					while(pertenece(padreGen1[aux1],genD1))
+						aux1++;
+					genD1[i] = padreGen1[aux1];
 				}
 				//Mirando al hijo2
 				if(!pertenece(padreGen2[i],genD2)){
 					genD2[i]=padreGen2[i];
+				}else{
+					while(pertenece(padreGen2[aux2],genD2))
+						aux2++;
+					genD2[i] = padreGen2[aux2];
 				}
 			}
 		}
 		
-		// las que dan conflicto las cambiamos por su pareja
-		for(int i=0;i<genLength;i++){
-			if(genD1[i]==-1){//Da conflicto
-				int simboloQueQueremosMeter = padreGen1[i];
-				int parejaDelSimbolo = parejaH1[simboloQueQueremosMeter];
-				genD1[i]=parejaDelSimbolo;
-			}
-			if(genD2[i]==-1){
-				int simboloQueQueremosMeter = padreGen2[i];
-				int parejaDelSimbolo = parejaH2[simboloQueQueremosMeter];
-				genD2[i]=parejaDelSimbolo;
-			}
-		}
-		
 		GAStudentGene genP1 = (GAStudentGene) parent1.getGene();
+		GAStudentGene genP2 = (GAStudentGene) parent2.getGene();
+		
+		checkear(padreGen1);
+		checkear(padreGen2);
 		checkear(genD1);
 		checkear(genD2);
-		GAStudentGene genP2 = (GAStudentGene) parent2.getGene();
+				
 		GAStudentCromosome[] descendientes= new GAStudentCromosome[2];
 		descendientes[0]= new GAStudentCromosome();
 		descendientes[0].setGene(new GAStudentGene(genD1, genP1.getNumberStudents(), genP1.getGroupSize(), genP1.getResultAverage(), genP1.getAlphaValue()));
@@ -87,14 +84,24 @@ public class GAPartialStudentPairing extends IGACross{
 		return descendientes;
 	}
 
-	public void checkear(int[] gn){
-		boolean argu=true;
-		int i=0;
-		while(i<gn.length & argu){
-			argu=pertenece(i, gn);
-			i++;
+	public static void checkear(int[] gn){
+		int[] aux = gn.clone();
+		for(int i=0;i<gn.length;i++){
+			int contador =0;
+			
+			while(IGACross.pertenece(i, aux)){
+				contador++;
+				aux[IGACross.getIndex(i, aux)]=-1;
+			}
+			
+			if(contador==0){
+				System.out.println(i+" no sale");
+			}
+			if(contador>1){
+				System.out.println(i+" sale repetido");				
+			}
+			
 		}
-		System.out.print(argu+""+(i-1));
 	}
 	
 }
