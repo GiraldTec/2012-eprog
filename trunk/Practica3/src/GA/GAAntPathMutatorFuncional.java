b5a6765ad3093bf91fd4ff2e4faac3fca42743ca
cacu2ggg <cacu2ggg@7db9f31f-8134-a458-1d93-559f5c0c0136>
2012-05-19 19:44:54 +0000

    Arreglada una tonteria del cruce y terminada la mutator funcional

--- trunk/Practica3/src/GA/GAAntPathMutatorFuncional.java
+++ trunk/Practica3/src/GA/GAAntPathMutatorFuncional.java
@@ -1,5 +1,8 @@
 package GA;
 
+import java.util.HashSet;
+import java.util.Iterator;
+
 import GACore.IGACromosome;
 import GACore.IGAMutator;
 import GACore.IGARandom;
@@ -19,7 +22,33 @@ public class GAAntPathMutatorFuncional extends IGAMutator{
 	}
 
 	private GAProgramTree getNodoFuncionalAleatorio(GAProgramTree treeP) {
-		// TODO Auto-generated method stub
-		return null;
+		HashSet<GAProgramTree> hashFuncionales = formHashFuncionales(treeP);
+		Iterator<GAProgramTree> iterador= hashFuncionales.iterator();
+		int i=0;
+		int pto1= IGARandom.getRInt(hashFuncionales.size());
+		while(i<pto1 && iterador.hasNext()){
+			iterador.next();
+			i++;
+		}
+		return iterador.next();
+	}
+	
+	public HashSet<GAProgramTree> formHashFuncionales(GAProgramTree treeP) {
+		HashSet<GAProgramTree> resNodos = new HashSet<GAProgramTree>();
+		formHashFuncionalesR(resNodos,treeP);
+		return resNodos;
+	}
+	
+	public void formHashFuncionalesR(HashSet<GAProgramTree> nodos, GAProgramTree treeP){
+		if(treeP.getLeftSon()==null &&
+				treeP.getCenterSon()==null &&
+					treeP.getRigthSon()==null){} // Si se trata de una hoja
+		else{// si es un nodo funcional
+			nodos.add(treeP);
+			formHashFuncionalesR(nodos, treeP.getLeftSon());
+			if(treeP.getCenterSon()!=null)
+				formHashFuncionalesR(nodos, treeP.getCenterSon());
+			formHashFuncionalesR(nodos, treeP.getRigthSon());
+		}
 	}
 }
