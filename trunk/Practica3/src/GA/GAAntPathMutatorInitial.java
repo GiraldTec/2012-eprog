a920e7feee79486b5fea6f1f90b5da527a96c445
cacu2ggg <cacu2ggg@7db9f31f-8134-a458-1d93-559f5c0c0136>
2012-05-19 23:20:43 +0000

    Arreglado un poco más el cromosoma, y añadida la mutacion inicial, pero hay que estudiarla un poco en mayor profundidad

--- trunk/Practica3/src/GA/GAAntPathMutatorInitial.java
+++ trunk/Practica3/src/GA/GAAntPathMutatorInitial.java
@@ -1,7 +1,11 @@
 package GA;
 
+import java.util.HashSet;
+import java.util.Iterator;
+
 import GACore.IGACromosome;
 import GACore.IGAMutator;
+import GACore.IGARandom;
 
 public class GAAntPathMutatorInitial extends IGAMutator{
 
@@ -10,7 +14,37 @@ public class GAAntPathMutatorInitial extends IGAMutator{
 	public Boolean mutate(IGACromosome cromosoma, double prob) {
 		System.out.print("entraamos a mutar");
 		Boolean res = new Boolean(false);
-
+		HashSet<GAProgramTree> hashNodo = formHash(((GAAntPathCromosome)cromosoma).getTreeP());
+		GAProgramTree nodoTerminal = getNodo(hashNodo,IGARandom.getRInt(hashNodo.size()));
+		if(IGARandom.getRDouble()<= prob){
+			//// Mutacion de arbol!! xD ¿QUE VALORES METEMOS AQUIi¡?
+			GAAntPathCromosome.initTree(nodoTerminal, minD, maxD);
+			res=true;
+		}
 		return res;
+	}
+	
+	public HashSet<GAProgramTree> formHash(GAProgramTree treeP) {
+		HashSet<GAProgramTree> resNodos = new HashSet<GAProgramTree>();
+		formHashRecursivo(resNodos,treeP);
+		return resNodos;
+	}
+	
+	public void formHashRecursivo(HashSet<GAProgramTree> nodos, GAProgramTree treeP){
+		nodos.add(treeP);
+		if(treeP.getLeftSon()!=null) 
+			formHashRecursivo(nodos, treeP.getLeftSon());
+		if(treeP.getCenterSon()!=null)
+			formHashRecursivo(nodos, treeP.getCenterSon());
+		if(treeP.getRigthSon()!=null)
+			formHashRecursivo(nodos, treeP.getRigthSon());
+	}
+	
+	public GAProgramTree getNodo(HashSet<GAProgramTree> nodos, int pto1) {
+		int i=0;
+		Iterator<GAProgramTree> iterador = nodos.iterator();
+		while(iterador.hasNext()&i<pto1){ iterador.next(); i++;}
+		return iterador.next();
+
 	}
 }
